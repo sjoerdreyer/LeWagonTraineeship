9181582fc61ef4d0500ee8a3c84bd1ba90e4dec3
Sjoerd de Wit <94071305+sjoerdreyer@users.noreply.github.com>
2022-07-13 15:45:21 +0200

    Delete 01-Programming-Basics directory

--- 01-Programming-Basics/05-System-Parameters/args.py
+++
@@ -1,7 +0,0 @@
-# pylint: disable=missing-module-docstring,missing-function-docstring
-
-# args.py
-import sys
-
-print('Number of arguments:', len(sys.argv), 'arguments.')
-print('Argument List:', str(sys.argv))
--- 01-Programming-Basics/08-Optional-Sudoku-Solver/sudoku.py
+++
@@ -1,5 +0,0 @@
-# pylint: disable=missing-docstring
-
-def sudoku_solver(grid):
-    """Sudoku solver"""
-    pass  # YOUR CODE HERE
